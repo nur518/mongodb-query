bbe1ca27ba3b429fb7ee6826828346cc3d0b44d4
nur518 <99117333+nur518@users.noreply.github.com>
2022-06-06 18:04:11 +0600

    array push | pull

--- mongodb-query.js
+++ mongodb-query.js
@@ -5,3 +5,9 @@
       "$or": [{ firstName: { $regex: new RegExp(req.query.name, 'i') } }, { lastName: { $regex: new RegExp(req.query.name, 'i') } }]
     }
   )
+
+  // pull object from array by (object property condition)
+  await DriveShare.findOneAndUpdate({ folderIds: { $all: folderIds }, "sharedUsers.email": email }, { $pull: { sharedUsers: { email: email } } }, { new: true });
+
+  //push multiple item in array
+  await folderShared.updateOne({ $push: { sharedUsers: { $each: sharedUsers } } }, { new: true });
